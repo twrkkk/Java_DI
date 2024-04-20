2fba90958b6f1917abb42d17de62a96bd2bc613d
twrkkk <zhenya.shein.03@gmail.com>
2024-04-20 12:49:36 +0300

    add java doc

--- src/Injector.java
+++ src/Injector.java
@@ -4,23 +4,37 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Properties;
 
+/**
+ * The Injector class is responsible for dependency injection based on annotations.
+ */
 public class Injector {
+    /**
+     * Path to the properties file containing interface-implementation mappings.
+     */
     private String pathToProperties;
-    public Injector(String pathToProperties)
-    {
+
+    /**
+     * Constructs an Injector object with the specified path to properties file.
+     *
+     * @param pathToProperties the path to the properties file
+     */
+    public Injector(String pathToProperties) {
         this.pathToProperties = pathToProperties;
     }
 
-    public<T> T inject(T impl)
-    {
+    /**
+     * Injects dependencies into the provided implementation object based on annotations.
+     *
+     * @param <T>  the type of the implementation
+     * @param impl the implementation object to inject dependencies into
+     * @return the implementation object with dependencies injected
+     */
+    public <T> T inject(T impl) {
         Properties properties = loadProperties(pathToProperties);
 
-        for (Field field : impl.getClass().getDeclaredFields())
-        {
-            if(field.isAnnotationPresent(AutoInjectable.class))
-            {
+        for (Field field : impl.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(AutoInjectable.class)) {
                 Class<?> fieldType = field.getType();
-
                 String interfaceName = properties.getProperty(fieldType.getName());
 
                 try {
@@ -30,22 +44,25 @@ public class Injector {
                 } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
                     e.printStackTrace();
                 }
-
             }
         }
-
         return impl;
     }
-    public Properties loadProperties(String path)
-    {
+
+    /**
+     * Loads properties from the specified properties file.
+     *
+     * @param path the path to the properties file
+     * @return properties loaded from the file
+     */
+    public Properties loadProperties(String path) {
         Properties properties = new Properties();
         try {
             properties.load(new FileInputStream(path));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return properties;
     }
-;
 }
+
